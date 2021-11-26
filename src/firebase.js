c4744d24d17992cb61330001242b2eb43d500100
Shashank <shaank1818@gmail.com>
2021-11-26 21:52:17 +0530

    Cleaned firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import firebase from 'firebase/app'
 import 'firebase/firebase-firestore';
 
 const firebaseConfig = {
-    apiKey: "***REMOVED***",
-    authDomain: "***REMOVED***",
-    projectId: "***REMOVED***",
-    storageBucket: "***REMOVED***.appspot.com",
-    messagingSenderId: "***REMOVED***",
-    appId: "1:***REMOVED***:web:c5491edf90a4ffe65ba5ef",
-    measurementId: "***REMOVED***"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
   };
 
 firebase.initializeApp(firebaseConfig);
