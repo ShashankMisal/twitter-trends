c059d90b48a38ba50ebb87492a7e16ebc1f12221
Shashank <shaank1818@gmail.com>
2021-11-26 22:46:41 +0530

    fixed createProxyMiddleware

--- src/setUpProxy.js
+++ src/setUpProxy.js
@@ -2,9 +2,9 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
     app.use(
-        "/1.1/trends/place.json?id=23424848", createProxyMiddleware({
+        createProxyMiddleware("/1.1/trends/place.json?id=23424848", {
             target: "https://api.twitter.com",
             changeOrigin: true,
-        })
-    )
+        }
+    ))
 }
